b6b2d493b97d39eb2382ab56476e5363ca03fbd9
sxarp <arise.kuriya.401@gmail.com>
2017-09-03 19:05:36 +0900

    define basis functional tools

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
+function foldl(array, init, fun) {
+  var ret_val = init;
+  for (i = 0; i < array.length; i++) { 
+    ret_val = fun(ret_val, array[i]);
+  }
+  return ret_val;
+};
+
+function head(array){
+  return array[0];
+};
+
+function tail(array){
+  return array.slice(1, array.length);
+}
+
+function fold(array, fun){
+  return foldl(tail(array), head(array), fun);
+};
+
+function map(array, fun){
+  return foldl(array, [], function(accumulated, val){
+    return accumulated.concat(fun(val));
+  });
+};
+
+function sum(a, b){
+  return a+b;
+}
+
+function create_category(category, number, probability) {
+  return {category: category, number: number, probability: probability};
+};
+function probability_of_category(category){
+  return category.number * category.probability;
+}
+
 new Vue({
-  el: '#vue-app',
+  el: '#main',
   data: {
     name: 'Sxarp',
     google: 'https://www.google.co.jp/',
     age: 26,
     flag: false,
-    characters: [
-      {name: 'chino', age: 14},
-      {name: 'cocoa', age: 16},
-      {name: 'syaro', age: 16}
-    ]
+    categories: map([['SSR', 1, 0.1], ['レア', 1, 0.1], ['ハズレ', 1, 0.1]],
+      function(args){return create_category(args[0], args[1], args[2])})
   },
   methods: {
     greet: function(time){
